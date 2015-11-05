0ac78f1c8ff0d3fc1b7d967f5ca7b3a7213a2295
KnownBright <KnownBright@outlook.com>
2015-11-05 22:46:49 +0800

    个人出租物品列表

--- And/Controllers/Personal_RentalProducts.js
+++ And/Controllers/Personal_RentalProducts.js
@@ -1,34 +1,48 @@
-mui.init({
-	pullRefresh: {
-		container: '#pullrefresh',
-		down: {
-			callback: pulldownRefresh
-		},
-		up: {
-			contentrefresh: '正在加载...',
-			callback: pullupRefresh
-		}
-	}
-});
+mui.init();
+(function($mui) {
+	//阻尼系数
+	var deceleration = mui.os.ios ? 0.003 : 0.0009;
+	$mui('.mui-scroll-wrapper').scroll({
+		bounce: false,
+		indicators: true, //是否显示滚动条
+		deceleration: deceleration
+	});
+	$mui.ready(function() {
+		//循环初始化所有下拉刷新，上拉加载。
+		$mui('#divProductList').pullToRefresh({
+			down: {
+				callback: function() {
+					pulldownRefresh(this);
+				}
+			},
+			up: {
+				callback: function() {
+					pullupRefresh(this);
+				}
+			}
+		});
+	});
+})(mui);
 /**
  * 下拉刷新具体业务实现
  */
-function pulldownRefresh() {
+function pulldownRefresh(var_this) {
+		setTimeout(function() {
+			var $table = $('.mui-table-view');
+			var $cell = $('.mui-table-view-cell:eq(0)');
+			$table.append($cell.clone());
+			var_this.endPullDownToRefresh(); //refresh completed
+		}, 000);
+	}
+	/**
+	 * 上拉加载具体业务实现
+	 */
+
+function pullupRefresh(var_this) {
 	setTimeout(function() {
 		var $table = $('.mui-table-view');
 		var $cell = $('.mui-table-view-cell:eq(0)');
 		$table.append($cell.clone());
-		mui('#pullrefresh').pullRefresh().endPulldownToRefresh(); //refresh completed
-	}, 1500);
-}
-/**
- * 上拉加载具体业务实现
- */
-function pullupRefresh() {
-	setTimeout(function() {
-		var $table = $('.mui-table-view');
-		var $cell = $('.mui-table-view-cell:eq(0)');
-		$table.append($cell.clone());
-		mui('#pullrefresh').pullRefresh().endPulldownToRefresh(); //refresh completed
-	}, 1500);
+		var_this.endPullDownToRefresh(); //refresh completed
+	}, 000);
 }
