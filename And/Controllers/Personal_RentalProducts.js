1665d3a8890eedac37b4d4ac51d2d2610705ddae
KnownBright <KnownBright@outlook.com>
2015-11-21 17:53:39 +0400

    商品列表与编辑

--- And/Controllers/Personal_RentalProducts.js
+++ And/Controllers/Personal_RentalProducts.js
@@ -1,48 +1,19 @@
 mui.init();
 (function($mui) {
-	//阻尼系数
-	var deceleration = mui.os.ios ? 0.003 : 0.0009;
-	$mui('.mui-scroll-wrapper').scroll({
-		bounce: false,
-		indicators: true, //是否显示滚动条
-		deceleration: deceleration
-	});
-	$mui.ready(function() {
-		//循环初始化所有下拉刷新，上拉加载。
-		$mui('#divProductList').pullToRefresh({
-			down: {
-				callback: function() {
-					pulldownRefresh(this);
-				}
-			},
-			up: {
-				callback: function() {
-					pullupRefresh(this);
-				}
+	$mui.init({
+		swipeBack: false,
+		statusBarBackground: '#f7f7f7',
+		gestureConfig: {
+			doubletap: true
+		},
+		subpages: [{
+			id: 'list',
+			url: 'ProductsList.html',
+			styles: {
+				top: '70px',
+				bottom: 0,
+				bounce: 'vertical'
 			}
-		});
+		}]
 	});
 })(mui);
-/**
- * 下拉刷新具体业务实现
- */
-function pulldownRefresh(var_this) {
-		setTimeout(function() {
-			var $table = $('.mui-table-view');
-			var $cell = $('.mui-table-view-cell:eq(0)');
-			$table.append($cell.clone());
-			var_this.endPullDownToRefresh(); //refresh completed
-		}, 000);
-	}
-	/**
-	 * 上拉加载具体业务实现
-	 */
-
-function pullupRefresh(var_this) {
-	setTimeout(function() {
-		var $table = $('.mui-table-view');
-		var $cell = $('.mui-table-view-cell:eq(0)');
-		$table.append($cell.clone());
-		var_this.endPullDownToRefresh(); //refresh completed
-	}, 000);
-}
